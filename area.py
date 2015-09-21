1ae58bcd20e960a3f3feda2a293037c529f31932
n.strelkov <nikita.strelkov@gmail.com>
2015-09-22 00:08:45 +0500

    First lesson on hexlet.io - DONE!

---
+++ area.py
@@ -0,0 +1,29 @@
+__author__ = 'strelkov'
+
+import math
+# BEGIN (write your solution here)
+
+x = input('x = ')
+figure = input('figure is: ')
+
+try:
+    x = float(x)
+    figure = str(figure)
+
+except:
+    print('error: invalid types')
+    exit(1)
+
+if x <= 0:
+    print('error: x must be positive')
+    exit(1)
+
+if figure == 'square':
+    print('area is %.2f' % (x * x))
+
+elif figure == 'circle':
+    print('area is %.2f' % (math.pi * x * x))
+
+else:
+    print('error: unknown figure')
+    exit(1)
