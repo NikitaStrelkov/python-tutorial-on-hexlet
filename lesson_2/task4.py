d0e6aac2c566f80748472f108eff7c59a30b4018
n.strelkov <nikita.strelkov@gmail.com>
2015-09-27 01:26:54 +0500

    lesson 2 task 4

---
+++ lesson_2/task4.py
@@ -0,0 +1,43 @@
+__author__ = 'strelkov'
+
+
+def task(seq):
+    """
+    Функция принимает последовательность (список, кортеж или множество).
+    Вернуть словарь вида {V: N}, где V -- элемент последовательности,
+    N -- сколько раз элемент встречается в последовательности.
+
+    Пример:
+    >>> task([10, 20, 20, 21, 30, 21])
+    {10: 1, 20: 2, 21: 2, 30: 1}
+    """
+    # BEGIN
+    res = {};
+    seq = list(seq)
+    for val in seq:
+        if not val in res:
+            res[val] = seq.count(val)
+    return res
+    # END
+
+
+def _test_task(seq, expected):
+    assert task(seq) == expected
+
+
+def test_ok():
+    _test_task([10, 20, 20, 21, 30, 21],
+               {10: 1, 20: 2, 21: 2, 30: 1})
+
+
+def test_empty():
+    _test_task((), {})
+
+
+def test_single():
+    _test_task({42}, {42: 1})
+
+
+def test_types():
+    _test_task((6, True, None, None, 1.0001, 1.0001, 'None', 6, 6),
+               {True: 1, None: 2, 1.0001: 2, 'None': 1, 6: 3})
